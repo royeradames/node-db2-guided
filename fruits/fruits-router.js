aaf732429d62c4866c9f529be9354f9adb07314c
royeradames <royeraadames@gmail.com>
2020-09-08 13:19:13 -0400

    going to work with migrations

--- fruits/fruits-router.js
+++ fruits/fruits-router.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const knex = require('knex');
 
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: './data/produce.db3'
-  },
-  useNullAsDefault: true
-});
+const db = require('../data/connections')
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   db('fruits')
   .then(fruits => {
-    res.json(fruits); 
+    res.status(200).json(fruits); 
   })
   .catch (err => {
     res.status(500).json({ message: 'Failed to retrieve fruits' });
@@ -36,6 +30,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   const fruitData = req.body;
   db('fruits').insert(fruitData)
+  .returning('id')//tomake it work with postgres as well as sqlite
   .then(ids => {
     db('fruits').where({ id: ids[0] })
     .then(newFruitEntry => {
---
+++ knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',//the database driver for sqlite
+    connection: {
+      filename: './data/produce.db3'// from root folder
+    },
+  useNullAsDefault: true //need for sqLite
+},
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
